fafecf4138461f10b3589971956a5ce5aa7a2551
phael42 <phaelgazeta@gmail.com>
2025-08-26 11:22:51 -0300

    Update parte2.js

--- parte2.js
+++ parte2.js
@@ -1,21 +1,36 @@
-const usuarios = [
-    {usuario: "Yue", senha: "3456"},
-    {usuario: "Sola", senha: "7890"},
-    {usuario: "Beatriz", senha: "1290"},
-];
-const loginDigitado = document.getElementById("loginform");
-const senhaDigitado = document.getElementById("mensagem");
-const mensagem = document.getElementById("mensagem");
-let encontrado = true;
+function verificarLogin() { 
+ const usuarios = [
+    { usuario: "Yue", senha: "3456" },
+    { usuario: "Sola", senha: "7890" },
+    { usuario: "Beatriz", senha: "1290" },
+  ];
+  const loginDigitado = document.getElementById("usuario").value;
+  const senhaDigitado = document.getElementById("senha").value;
+  const mensagem = document.getElementById("mensagem");
+  let encontrado = false;
 
-for (let i = 0; i < usuarios.length; i++) {
-    if(login === usuarios.login[i] && senha === usuarios.senha [i]){
-       encontrado = false;
-       mensagem.innerHTML= "Login bem sucedido ," + usuarios[1].login + "!";
+  for (let i = 0; i < usuarios.length; i++) {
+    if (
+      loginDigitado === usuarios[i].usuario &&
+      senhaDigitado === usuarios[i].senha
+    ) {
+      encontrado = true;
+      mensagem.innerHTML = "Login bem sucedido ," + usuarios[i].usuario + "!";
     }
-   }
-   
-   if(encontrado){
+  }
+
+  if (!encontrado) {
+
+    setTimeout(() => {
     mensagem.innerHTML = "Login ou senha incorretos, por favor tente novamente";
-   }
-    
+    mensagem.innerHTML = "Login bem sucedido";
+    titulo.innerHTML = "Pagina de login";
+  }, 6000);
+
+  document.getElementById("usuario").value = "";
+    document.getElementById("senha").value = "";
+    window.location.href = "https://pt.wikipedia.org/wiki/Mitologia_grega";
+
+  }
+}
+ 
